e5ff4cdfbd3677fc960ec0ea6d223793b45940b2
Shivam Sharma <sharma.shivam2384@gmail.com>
2025-09-18 23:30:00 +0530

    Updated

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -8,46 +8,44 @@ let minutes = 0;
 let seconds = 0;
 let miliseconds = 0;
 let timerId = null;
-
-function formatym(time){
-    return time < 10 ? `0${time}` : time
+function Displayupdated(time){
+    return  time < 10 ? `0${time}`:time
 }
+
 startbtn.addEventListener('click',()=>{
-    if(timerId !== null){
-        return 
-    }
+
     timerId = setInterval(()=>{
+ if(timerId !== null){
         miliseconds++;
         if(miliseconds == 100){
             miliseconds = 0;
             seconds++
-    } if(seconds == 60){
-        seconds = 0
-        minutes++;
-    }if(minutes == 60){
-        minutes = 0;
-        hour++
-    }
-        let formatedhour = formatym(hour)
-        let formatedmin = formatym(minutes)
-        let formatedsec = formatym(seconds)
-        let formatedmili = formatym(miliseconds)
+         }if(seconds == 60){
+            seconds = 0;
+            minutes++;
+         }if(minutes == 60){
+            minutes = 0;
+            hour++;
+         }
+     }
+        let  updatehour = Displayupdated(hour)
+        let  updatemin = Displayupdated(minutes)
+        let  updatesec = Displayupdated(seconds)
+        let  updatemili = Displayupdated(miliseconds)
 
-
-
-    timerdisplay.textContent = `${formatedhour} : ${formatedmin} : ${formatedsec} : ${formatedmili}`
-    },10)
+     timerdisplay.textContent = `${updatehour} : ${updatemin} : ${updatesec} : ${updatemili}`
+   },10)
 })
 stopbtn.addEventListener('click',()=>{
     clearInterval(timerId);
-    timerId=null
+    timerId = 0;
 })
 resetbtn.addEventListener('click',()=>{
-    clearInterval(timerId);
-    timerId = null;
-    hour =0;
-    minutes=0;
-    seconds =0;
-    miliseconds=0;
-    timerdisplay.textContent =`00 : 00 : 00 : 00`;
+    clearInterval(timerId)
+    timerId = 0;
+    hour=0
+    minutes =0;
+    seconds = 0;
+    miliseconds =0;
+    timerdisplay.textContent = `00 : 00 : 00 : 00`
 })
